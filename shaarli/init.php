e691500536db8a6301081e449fe4dbd723f7572b
Jean-Christophe Saad-Dupuy <saad.dupuy@gmail.com>
2013-03-25 20:16:52 +0100

    Updated version format

--- shaarli/init.php
+++ shaarli/init.php
@@ -6,7 +6,7 @@ class Shaarli extends Plugin {
   private $host;
 
   function about() {
-    return array("1.7.5",
+    return array("1.75",
 		 "Shaare your links ! (Sebsauvage Shaarli : http://sebsauvage.net/wiki/doku.php?id=php:shaarli )",
 		 "jc.saaddupuy");
   }
