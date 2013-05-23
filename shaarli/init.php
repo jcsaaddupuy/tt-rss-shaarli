798675a1d5912d73570e6e90b0db25cff50ede54
Jean-Christophe Saad-Dupuy <saad.dupuy@gmail.com>
2013-05-23 20:47:57 +0200

    Added 1.7.9 support

--- shaarli/init.php
+++ shaarli/init.php
@@ -6,21 +6,21 @@ class Shaarli extends Plugin {
   private $host;
 
   function about() {
-    return array("1.7.5",
-		 "Shaare your links ! (Sebsauvage Shaarli : http://sebsauvage.net/wiki/doku.php?id=php:shaarli )",
-		 "jc.saaddupuy");
+    return array("1.7.ç",
+      "Shaare your links ! (Sebsauvage Shaarli : http://sebsauvage.net/wiki/doku.php?id=php:shaarli )",
+      "jc.saaddupuy");
   }
 
   function init($host) {
-    $this->link = $host->get_link();
     $this->host = $host;
+    $this->dbh = $host->get_dbh();
 
     $host->add_hook($host::HOOK_ARTICLE_BUTTON, $this);
     $host->add_hook($host::HOOK_PREFS_TAB, $this);
   }
 
   function save() {
-    $shaarli_url = db_escape_string($this->link,$_POST["shaarli_url"]);
+    $shaarli_url = db_escape_string($_POST["shaarli_url"]);
     $this->host->set($this, "shaarli", $shaarli_url);
     echo "Value set to $shaarli_url";
   }
@@ -40,16 +40,16 @@ class Shaarli extends Plugin {
     print "<form dojoType=\"dijit.form.Form\">";
 
     print "<script type=\"dojo/method\" event=\"onSubmit\" args=\"evt\">
-           evt.preventDefault();
-           if (this.validate()) {
-               console.log(dojo.objectToQuery(this.getValues()));
-               new Ajax.Request('backend.php', {
-                                    parameters: dojo.objectToQuery(this.getValues()),
-                                    onComplete: function(transport) {
-                                         notify_info(transport.responseText);
-                                    }
-                                });
-           }
+      evt.preventDefault();
+    if (this.validate()) {
+      console.log(dojo.objectToQuery(this.getValues()));
+      new Ajax.Request('backend.php', {
+        parameters: dojo.objectToQuery(this.getValues()),
+          onComplete: function(transport) {
+            notify_info(transport.responseText);
+  }
+  });
+  }
            </script>";
 
     print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"op\" value=\"pluginhandler\">";
@@ -57,7 +57,7 @@ class Shaarli extends Plugin {
     print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"plugin\" value=\"shaarli\">";
     print "<table width=\"100%\" class=\"prefPrefsList\">";
         print "<tr><td width=\"40%\">".__("Shaarli url")."</td>";
-	print "<td class=\"prefValue\"><input dojoType=\"dijit.form.ValidationTextBox\" required=\"1\" name=\"shaarli_url\" regExp='^(http|https)://.*' value=\"$value\"></td></tr>";
+        print "<td class=\"prefValue\"><input dojoType=\"dijit.form.ValidationTextBox\" required=\"1\" name=\"shaarli_url\" regExp='^(http|https)://.*' value=\"$value\"></td></tr>";
     print "</table>";
     print "<p><button dojoType=\"dijit.form.Button\" type=\"submit\">".__("Save")."</button>";
 
@@ -75,22 +75,25 @@ class Shaarli extends Plugin {
   }
 
   function getShaarli() {
-    $id = db_escape_string($this->link,$_REQUEST['id']);
+    $id = db_escape_string($_REQUEST['id']);
 
-    $result = db_query($this->link, "SELECT title, link
-		      FROM ttrss_entries, ttrss_user_entries
-		      WHERE id = '$id' AND ref_id = id AND owner_uid = " .$_SESSION['uid']);
+    $result = $this->dbh->query("SELECT title, link
+                      FROM ttrss_entries, ttrss_user_entries
+                      WHERE id = '$id' AND ref_id = id AND owner_uid = " .$_SESSION['uid']);
 
     if (db_num_rows($result) != 0) {
       $title = truncate_string(strip_tags(db_fetch_result($result, 0, 'title')),
-			       100, '...');
+                               100, '...');
       $article_link = db_fetch_result($result, 0, 'link');
     }
 
     $shaarli_url = $this->host->get($this, "shaarli");
 
     print json_encode(array("title" => $title, "link" => $article_link,
-			    "id" => $id, "shaarli_url" => $shaarli_url));
+                            "id" => $id, "shaarli_url" => $shaarli_url));
   }
+   function api_version() {
+         return 2;
+      }
 }
 ?>
